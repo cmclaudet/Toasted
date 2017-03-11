af0f3fe994dd89542cada649ffd61390ec8cebca
Catherine Claudet <cmclaudet@gmail.com>
2017-03-11 18:00:27 +0100

    Added toast spawner
    - toasts spawn randomly every 4 seconds

--- Assets/scripts/toastSpawner.cs
+++ Assets/scripts/toastSpawner.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class toastSpawner : MonoBehaviour {
 
 	public GameObject toastPrefab;
-	private Transform[] spawnPoints;
+	public float spawnFrequency;
+
+	private float timeSinceSpawn;
 
 	// Use this for initialization
 	void Start () {
-		SpawnToast();		
+		timeSinceSpawn = 0;
 	}
 				
 	// Update is called once per frame
 	void Update () {
-		
+		timeSinceSpawn += Time.deltaTime;
+
+		if (timeSinceSpawn >= spawnFrequency) {
+			SpawnToast ();
+			timeSinceSpawn = 0;
+		//	inactivateToasts ();
+		}
 	}
 
 	void SpawnToast() {
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-		Instantiate (toastPrefab, transform.position, Quaternion.identity);
+		int xIndex = Random.Range (1, 9);
+		int yIndex = Random.Range (1, 7);
+		Vector3 screenPosition = new Vector3 (xIndex + 0.5f, yIndex + 0.5f) * 96;
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint (screenPosition);
+		worldPosition = new Vector3 (worldPosition.x, worldPosition.y, 10.0f);
+		Instantiate (toastPrefab, worldPosition, Quaternion.identity);
 	}
+
+
 
 }
